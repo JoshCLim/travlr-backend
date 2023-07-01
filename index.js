8b928185864716f1f139f667f9eab68c5e393024
Prayag <prayag1601@gmail.com>
2023-07-01 16:14:51 +1000

    add postgres conn

--- index.js
+++ index.js
@@ -1,7 +1,21 @@
-const express = require('express')
-const app = express()
-const port = 3000
+import express from 'express';
+import dotenv from 'dotenv';
+dotenv.config()
 
+const app = express()
+const port = 8000
+
+import pkg from 'pg';
+
+const client = new pkg.Client({
+    user: process.env.POSTGRES_USER,
+    host: process.env.POSTGRES_HOST,
+    database: process.env.DBNAME,
+    password: process.env.POSTGRES_PASSWORD,
+    port: process.env.POSTGRES_PORT,
+  })
+await client.connect()
+ 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
