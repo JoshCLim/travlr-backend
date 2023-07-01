85948cd1009d4978960c9be69007c8e5f5691b28
Prayag <prayag1601@gmail.com>
2023-07-01 18:58:45 +1000

    prevent password leak

--- routes/users.js
+++ routes/users.js
@@ -1,14 +1,18 @@
 import express from "express";
 import dotenv from "dotenv";
 import db from "../index.js";
+import bcrypt from "bcrypt";
+
 dotenv.config();
 
 const router = express.Router();
+const saltRounds = 10
+const password = process.env.BCRYPT_PASSWORD;
 
 // Get all users
 router.get("/", async (req, res) => {
   try {
-    const { rows } = await db.query("SELECT * FROM users");
+    const { rows } = await db.query("SELECT id, name, email, photo_id, bio, last_location FROM users");
     res.json(rows);
   } catch (err) {
     console.error(err);
@@ -37,26 +41,34 @@ router.post("/", async (req, res) => {
   const {
     name,
     email,
-    password_hash,
+    password,
     photo_id,
     bio,
-    invalidate_tokens_before,
     last_location,
   } = req.body;
   try {
-    const { rows } = await db.query(
-      "INSERT INTO users (name, email, password_hash, photo_id, bio, invalidate_tokens_before, last_location) VALUES ($1, $2, $3, $4, $5, $6, $7) RETURNING *",
-      [
-        name,
-        email,
-        password_hash,
-        photo_id,
-        bio,
-        invalidate_tokens_before,
-        last_location,
-      ]
-    );
-    res.status(201).json(rows[0]);
+
+    bcrypt
+      .genSalt(saltRounds)
+      .then(salt => {
+        return bcrypt.hash(password, salt)
+      })
+      .then(async password_hash => {
+        res.status(400).send("");
+        const { rows } = await db.query(
+          "INSERT INTO users (name, email, password_hash, photo_id, bio, last_location) VALUES ($1, $2, $3, $4, $5, $6) RETURNING *",
+          [
+            name,
+            email,
+            password_hash,
+            photo_id,
+            bio,
+            last_location,
+          ]
+        );
+        res.status(201).json(rows[0]);
+      })
+      .catch(err => console.error(err.message))
   } catch (err) {
     console.error(err);
     res.status(500).send("Server Error");
@@ -68,22 +80,18 @@ router.put("/:id", async (req, res) => {
   const {
     name,
     email,
-    password_hash,
     photo_id,
     bio,
-    invalidate_tokens_before,
     last_location,
   } = req.body;
   try {
     const { rows } = await db.query(
-      "UPDATE users SET name = $1, email = $2, password_hash = $3, photo_id = $4, bio = $5, invalidate_tokens_before = $6, last_location = $7 WHERE id = $8 RETURNING *",
+      "UPDATE users SET name = $1, email = $2, photo_id = $3, bio = $4, last_location = $5 WHERE id = $6 RETURNING *",
       [
         name,
         email,
-        password_hash,
         photo_id,
         bio,
-        invalidate_tokens_before,
         last_location,
         req.params.id,
       ]
